64f4e94398d9f30b2b595af68a2cb28f984390c7
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-11-21 11:27:27 +0100

    add methods documentation

--- CarWashing/include/Button.h
+++ CarWashing/include/Button.h
@@ -2,7 +2,14 @@
 #define __BUTTON__
 class Button {
     public:
+        /**
+         * Class contructor.
+         * @param pin the pin number where button is connect
+        */
         Button(int pin);
+        /**
+         * Function to check if a button is pressed. 
+        */
         bool isPressed();
     protected:
         int pin; 
