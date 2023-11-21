54cc660b77b4e4f74dfbae5d3b3ab1945665be57
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-11-21 11:34:54 +0100

    Added other documentations

--- CarWashing/include/Led.h
+++ CarWashing/include/Led.h
@@ -2,9 +2,23 @@
 #define __LED__
 class Led {
     public:
+        /**
+         * Class contructor.
+         * @param pin number of the pin where the led is connect.
+        */
         Led(int pin);
+        /**
+         * Function to turn on the led.
+        */
         void switchOn();
+        /**
+         * Function to turn off the led.
+        */
         void switchOff();
+        /**
+         * Function to check if the is on.
+         * @return true if on, false otherwise
+        */
         bool isOn();
     protected:
         int pin;
