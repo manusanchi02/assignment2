0c8126a9d6d2080a32113a587ff6872a01a55ec2
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-11-22 18:43:42 +0100

    Create leaving task class

---
+++ CarWashing/src/LeavingTask.h
@@ -0,0 +1,46 @@
+#ifndef __LEAVINGTASK__
+#define __LEAVINGTASK__
+#include <Arduino.h>
+#include "Task.h"
+#include "Led.h"
+#include "LcdMonitor.h"
+#include "Gate.h"
+#include "Sonar.h"
+
+class LeavingTask : public Task {
+    public:
+        /**
+         * Class constructor.
+         * @param ledPin1 number of the first led pin
+         * @param ledPin2 number of the second led pin
+         * @param rows number of lcd rows
+         * @param column number of lcd columns
+         * @param gatePin number of the gate pin
+        */
+        LeavingTask(int ledPin1, int ledPin2, int rows, int column, int gatePin, int echoPinIn, int trigPinOut, float velocitaSuono);
+        /**
+         * Init method.
+         * @param period task period
+        */
+        void init(int period);
+        /**
+         * Tick method.
+        */
+        void tick();
+    protected:
+        int ledPin1;
+        int ledPin2;
+        int rows;
+        int column;
+        int gatePin;
+        int echoPinIn;
+        int trigPinOut;
+        float velocitaSuono;
+        Led* led1;
+        Led* led2;
+        LcdMonitor* lcd;
+        Gate* gate;
+        Sonar* distanceSensor;
+};
+
+#endif
