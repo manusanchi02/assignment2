57880af1dee25e26d0f8397f46ffea7a22489d15
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-11-23 15:23:56 +0100

    Add SleepingTask.cpp

--- CarWashing/src/SleepingTask.cpp
+++ CarWashing/src/SleepingTask.cpp
@@ -7,6 +7,15 @@ SleepingTask::SleepingTask() {
 }
 
 void SleepingTask::init() {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    power_adc_disable();
+    power_spi_disable();
+    power_twi_disable();
+    sleep_mode();
+    // in this point arduino wake up
+    sleep_disable();
+    power_all_enable();
 
 }
 
