3626f333a3176b8013427976ff26e736bc1aa6a3
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-11-23 15:30:30 +0100

    Small Fix

--- CarWashing/src/SleepingTask.cpp
+++ CarWashing/src/SleepingTask.cpp
@@ -2,23 +2,31 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 
-SleepingTask::SleepingTask() {
+int counter; 
 
+SleepingTask::SleepingTask() {
+    counter = 0;
 }
 
 void SleepingTask::init() {
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    sleep_enable();
-    power_adc_disable();
-    power_spi_disable();
-    power_twi_disable();
-    sleep_mode();
-    // in this point arduino wake up
-    sleep_disable();
-    power_all_enable();
 
 }
 
 void SleepingTask::tick() {
-    
+
+    if(sleeping && counter < 1) {
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_enable();
+        power_adc_disable();
+        power_spi_disable();
+        power_twi_disable();
+        sleep_mode();
+        // in this point arduino wake up
+        sleep_disable();
+        power_all_enable();
+        counter++;
+    } else {
+        sleeping = false;
+        welcome = true;
+    }
 }
