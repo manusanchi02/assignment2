a84567830710b565bd70f2e9ae8fbd25c0ddd812
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-11-28 12:00:34 +0100

    Add GUI

---
+++ GuiTest.py
@@ -0,0 +1,37 @@
+import PySimpleGUI as sg
+
+errore = True
+currentTemp = 0
+totalWash = 0
+currentState = 'Sleeping'
+errors = 'No Errors'
+restartButton = sg.Button('Restart')
+
+sg.theme('LightBlue')  # Add a touch of color
+# All the stuff inside your window.
+defaultLayout = [  [sg.Text('Current Temperature:') ,sg.Text(currentTemp)],
+            [sg.Text('Total Car Washed:') ,sg.Text(totalWash)],
+            [sg.Text('Current State:') ,sg.Text(currentState)],
+            [sg.Text('Error:') ,sg.Text(errors)]]
+if errore == True:
+    defaultLayout.append([restartButton])
+
+loginLayout = [  [sg.Text('Please enter COM port')],
+            [sg.Text('COM Port', size=(15, 1)), sg.InputText()],
+            [sg.Button('Ok')]]
+
+
+# Create the Window
+window = sg.Window('Arduino Controller', loginLayout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    if event == 'Ok':
+        window.close()
+        window = sg.Window('Arduino Controller', defaultLayout)
+    if event == 'Restart':
+        print('Restarting')
+
+window.close()
