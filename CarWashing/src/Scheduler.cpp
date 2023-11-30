2444dc9dce4a9f96158c22c7031868959ceae457
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-11-30 11:50:15 +0100

    removed timerone and use millis

--- CarWashing/src/Scheduler.cpp
+++ CarWashing/src/Scheduler.cpp
@@ -2,19 +2,12 @@
 #include <TimerOne.h>
 #include "GlobalVariables.h"
 
-volatile bool timerFlag;
-
-void timerHandler(void){
-  //Serial.println("timerScattato");
-  timerFlag = true;
-}
+volatile long currentMillis = 0;
+volatile long timerFlag = false;
 
 void Scheduler::init(int basePeriod){
   this->basePeriod = basePeriod;
   timerFlag = false;
-  long period = 1000l*basePeriod;
-  Timer1.initialize(period);
-  Timer1.attachInterrupt(timerHandler);
   nTasks = 0;
 }
 
@@ -29,36 +22,14 @@ bool Scheduler::addTask(Task* task){
 }
 
 void Scheduler::schedule(){   
-  while (!timerFlag){/*long counter = TCNT1;
-  Serial.println(counter);*/
-  /*if(!timerFlag)
-  timerFlag = true;*//*
-  if(washing)
-  Serial.println("washing");
-  if(moving)
-  Serial.println("moving");
-  if(welcome)
-  Serial.println("welcome");
-  if(ready)
-  Serial.println("ready");
-  if(closing)
-  Serial.println("closing");
-  if(sleeping)
-  Serial.println("sleeping");
-  if(leaving)
-  Serial.println("leaving");
-  ;
-  if(!welcome && !moving && !washing && !ready && !closing && !sleeping && !leaving)
-  Serial.println("idle");
-  if(closing)
-  taskList[3]->tick();*/
+  while (!timerFlag){
+    if (millis() - currentMillis > basePeriod) {
+      currentMillis = millis();
+      timerFlag = true;
+    }
   }
 
   timerFlag = false;
-  Timer1.attachInterrupt(timerHandler, 1000l*basePeriod);
-  /*long counter = TCNT1;
-  Serial.println(counter);*/
-
 
   for (int i = 0; i < nTasks; i++){
     if (taskList[i]->updateAndCheckTime(basePeriod)){
