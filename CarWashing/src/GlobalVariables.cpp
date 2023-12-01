bcf6f811012825eb93415d823393910a52e2b5fe
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-12-01 14:09:13 +0100

    tests

--- CarWashing/src/GlobalVariables.cpp
+++ CarWashing/src/GlobalVariables.cpp
@@ -1,11 +1,11 @@
 #include "GlobalVariables.h"
-volatile bool welcome = false;
+volatile bool welcome = true;
 volatile bool moving =false;
 volatile bool ready = false;
 volatile bool washing = false;
 volatile bool leaving = false;
 volatile bool closing = false;
-volatile bool sleeping = true;
+volatile bool sleeping = false;
 volatile bool isNear = false;
 volatile bool gateState = false;
 volatile int carCounter = 0;
