724fc5e5ff4dfe29161c0e2b9d06a078ba226f83
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-12-01 10:42:29 +0100

    Update DELTA value in Gate.cpp and add delay(6)

--- CarWashing/src/Gate.cpp
+++ CarWashing/src/Gate.cpp
@@ -1,7 +1,7 @@
 #include "Gate.h"
 #include <Arduino.h>
 #include "GlobalVariables.h"
-#define DELTA 2
+#define DELTA 1
 
 
 Gate::Gate(int pin, int open, int close)
@@ -20,7 +20,7 @@ void Gate::setOpen()
         for(int i = 0; i < this->open; i++) {
             this->pos+=DELTA;
             this->servo.write(pos);
-            Serial.println(pos);
+            delay(6);
         }
         gateState = true;
         this->servo.detach();
@@ -35,7 +35,7 @@ void Gate::setClose()
         for(int a = 0; a < this->open; a++) {
             this->pos-=DELTA;
             this->servo.write(pos);
-            Serial.println(pos);
+            delay(6);
         }
         gateState = false;
         this->servo.detach();
