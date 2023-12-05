6c2673f97c076c3f1081a1dc5eba9c64081ff689
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-12-05 15:16:14 +0100

    Removed define and change first state in sleeping

--- CarWashing/src/GlobalVariables.h
+++ CarWashing/src/GlobalVariables.h
@@ -1,12 +1,5 @@
 // Various global variables used in the project for states and other purposes.
-#define N1 5000
-#define N2 2000
-#define N3 8000
-#define N4 3000
-#define N5 4000
-#define MAXDIST 0.30
-#define MINDIST 0.10
-#define ERROR_TEMPERATURE 50
+
 
 extern volatile bool isNear;
 extern volatile bool error;
